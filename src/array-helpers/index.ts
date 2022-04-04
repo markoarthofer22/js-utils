a887438663e0551649324c812e3b508ca229a8d4
Marko Arthofer <markoarthofer95@gmail.com>
2022-04-04 11:58:24 +0200

    feat: new methods for arrays

--- src/array-helpers/index.ts
+++ src/array-helpers/index.ts
@@ -5,6 +5,28 @@ export function first<T>(array: T[], n = 1): T | T[] {
     return array.filter((_, i: number) => i < n);
 }
 
+export function last<T>(array: T[], n = 1): T | T[] {
+    if (n === 1) return array[array.length - 1];
+    return array.filter((_, i) => array.length - i <= n);
+}
+
 export function sample<T>(array: T[]): T {
     return array[randomNumberBetween(0, array.length - 1)];
+}
+
+export function pluck<T extends Record<string, any>>(
+    array: T[],
+    key: keyof T
+): T | T[] {
+    return array.map((element) => element[key as number]);
+}
+
+export function groupBy<T extends Record<string, any>>(
+    array: T[],
+    key: keyof T
+): any {
+    return array.reduce((group: { [any: string]: any }, element) => {
+        const keyValue = element[key];
+        return { ...group, [keyValue]: [...(group[keyValue] ?? []), element] };
+    }, {});
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import { randomNumberBetween } from 'utils/helpers';
-import { sample, first } from 'array-helpers';
+import { sample, first, last, pluck, groupBy } from 'array-helpers';
 
-export { randomNumberBetween, sample, first };
+export { randomNumberBetween, sample, first, last, pluck, groupBy };
